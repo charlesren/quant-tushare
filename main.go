2e71d8672b1353a11561f2f48345c906f10d73ef
charlesren <renccn@gmail.com>
2019-07-12 23:00:41 +0800

    change main

--- main.go
+++ main.go
@@ -10,20 +10,12 @@ func main() {
 	dbconn := config.GetDB()
 	fmt.Print(dbconn)
 	params := make(map[string]string)
-	params["trade_date"] = "20190708"
+	//	params["trade_date"] = "20190708"
 	var fields []string
 	var api *tushare.TuShare
-	var resp tushare.APIResponse
 	resp, err := api.Daily(params, fields)
 	if err != nil {
 		err.Error()
 	}
-
-	resp.r2j
-}
-
-func (r tushare.APIResponse) r2j() (string, error) {
-	d = r.Data
-	f = d.Fields
-	fmt.Printf(f)
+	fmt.Print(resp)
 }
