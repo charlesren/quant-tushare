febe75a5ff7787d410e634d663e8ec8fe170aafa
charlesren <renccn@gmail.com>
2019-07-13 18:06:28 +0800

    change config

--- src/config/config.go
+++ src/config/config.go
@@ -37,17 +37,16 @@ func initDB() {
 	}
 	dbUri := fmt.Sprintf("host=%s port=%d dbname=%s user=%s password=%s sslmode=%s ", DBConfig.Host, DBConfig.DbPort, DBConfig.Dbname, DBConfig.Username, DBConfig.Password, DBConfig.Sslmode)
 	fmt.Println("Connecting: " + dbUri)
-	//var err error
 	dbConn, err = gorm.Open(DBConfig.Platform, dbUri)
 	if err != nil {
 		panic(err)
 	}
 
 	//    if config.ServerConfig.Env == DevelopmentMode { DB.LogMode(true) }
-	dbConn.DB().SetMaxIdleConns(DBConfig.MaxIdleConns)
-	dbConn.DB().SetMaxOpenConns(DBConfig.MaxOpenConns)
+	//dbConn.DB().SetMaxIdleConns(DBConfig.MaxIdleConns)
+	//dbConn.DB().SetMaxOpenConns(DBConfig.MaxOpenConns)
 	//user singular table in case y change to ies
-	dbConn.SingularTable(true)
+	//dbConn.SingularTable(true)
 	//	dbConn.Debug().AutoMigrate(&model.User{}, &model.Product{}, &model.Order{}, &model.OrderLine{})
 	//	dbConn.Model(&model.OrderLine{}).AddForeignKey("order_id", "orders(order_id)", "CASCADE", "CASCADE")
 	//	dbConn.Model(&model.OrderLine{}).AddForeignKey("product_id", "products(product_id)", "CASCADE", "CASCADE")
