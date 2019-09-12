4a7826b020e1825f11c56e549e30f7c6c2bb2496
charlesren <renccn@gmail.com>
2019-09-12 22:48:34 +0800

    use TushareModelFields func print fields

--- main.go
+++ main.go
@@ -5,38 +5,23 @@ import (
 	"log"
 	"quant-tushare/src/config"
 	"quant-tushare/src/tushare"
-	"time"
-
-	"github.com/jinzhu/gorm"
 )
 
 func main() {
 	db := config.GetDB()
 	defer db.Close()
 	api := tushare.New(tushare.TushareConfig.Token)
-	params := make(map[string]string)
-	var fields []string
-	today := time.Now().Format("20060102")
-	fmt.Printf("today is %v\n", today)
-	//params["trade_date"] = "20190708"
-	params["ts_code"] = "000002.SZ"
-	params["start_date"] = "20190707"
-	params["end_date"] = "20190711"
-	resp, err := api.GetDaily(params, fields)
+	params := make(tushare.Params)
+	var fields tushare.Fields
+	//today := time.Now().Format("20060102")
+	//params["ts_code"] = "000002.SZ"
+	//params["start_date"] = "20190707"
+	//params["end_date"] = today
+	resp, err := api.GetTushareData("stock_basic", params, fields)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(*resp)
-	for _, v := range resp.ParsingDaily() {
-		fmt.Println(v)
-		if err := db.Find(&v).Error; err != nil {
-			if err == gorm.ErrRecordNotFound {
-				db.Create(&v)
-			} else {
-				fmt.Println(err)
-			}
-		}
-	}
+	resp.TushareModelFields()
 	var daily tushare.Daily
 	//db.Select("trade_date, open").Find(&daily)
 	db.Where("trade_date = ?", "20190708").Find(&daily)
