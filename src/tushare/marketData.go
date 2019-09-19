89b6050ad904e0f0cd070a7f87d0a133ff8a367d
charlesren <renccn@gmail.com>
2019-09-19 22:39:40 +0800

    delete GetDaily func

--- src/tushare/marketData.go
+++ src/tushare/marketData.go
@@ -9,31 +9,6 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-// GetDaily 获取股票行情数据, 日线行情
-func (api *TuShare) GetDaily(params Params, fields Fields) (*APIResponse, error) {
-	// Check params
-	_, hasTsCode := params["ts_code"]
-	_, hasTradeDate := params["trade_date"]
-
-	// ts_code & trade_date required
-	if (!hasTsCode && !hasTradeDate) || (hasTsCode && hasTradeDate) {
-		return nil, fmt.Errorf("Need one argument ts_code or trade_date")
-	}
-
-	if dateFormat := IsDateFormat(params["trade_date"], params["start_date"], params["end_date"]); !dateFormat {
-		return nil, fmt.Errorf("please input right date format YYYYMMDD")
-	}
-
-	body := map[string]interface{}{
-		"api_name": "daily",
-		"token":    api.token,
-		"fields":   fields,
-		"params":   params,
-	}
-
-	return api.postData(body)
-}
-
 // GetTushareData use http post methord to get data from https://tushare.pro
 func (api *TuShare) GetTushareData(dataType string, params Params, fields Fields) (*APIResponse, error) {
 	/*
