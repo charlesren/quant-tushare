f357806581bc018ff047359b137c76f79e3f315b
charlesren <renccn@gmail.com>
2019-10-09 22:29:27 +0800

    add GetData and TushareTosina func

---
+++ src/sina/utils.go
@@ -0,0 +1,14 @@
+package sina
+
+const sinaURLPrefix = "http://hq.sinajs.cn/list"
+
+// GetData return real time stock data info
+func GetData(sotck string) Data {
+	var data Data
+	return data
+}
+
+// TushareToSina convert Stock name from tushare format to Sina format
+func TushareToSina(stock string) string {
+	return stock
+}
