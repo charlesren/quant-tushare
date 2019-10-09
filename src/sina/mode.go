b7a76081df2a582462acb7234e32f5d6fd4753fd
charlesren <renccn@gmail.com>
2019-10-09 21:31:29 +0800

    add sina Data

---
+++ src/sina/mode.go
@@ -0,0 +1,37 @@
+package sina
+
+// Data resp of sina api
+type Data struct {
+	Name       string
+	Open       float64
+	Close      float64
+	Now        float64
+	High       float64
+	Low        float64
+	Buy        float64
+	Sell       float64
+	Turnover   int
+	Volume     float64
+	Bid1Volume int
+	Bid1       float64
+	Bid2Volume int
+	Bid2       float64
+	Bid3Volume int
+	Bid3       float64
+	Bid4Volume int
+	Bid4       float64
+	Bid5Volume int
+	Bid5       float64
+	Ask1Volume int
+	Ask1       float64
+	Ask2Volume int
+	Ask2       float64
+	Ask3Volume int
+	Ask3       float64
+	Ask4Volume int
+	Ask4       float64
+	Ask5Volume int
+	Ask5       float64
+	Date       string
+	Time       string
+}
