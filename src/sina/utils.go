8c0e161ae0aac9f3c6310763eacd4748886bdcfa
rencc <renccn@gmail.com>
2019-10-10 16:19:16 +0800

    update TushareToSina func

--- src/sina/utils.go
+++ src/sina/utils.go
@@ -1,5 +1,9 @@
 package sina
 
+import (
+	"strings"
+)
+
 const sinaURLPrefix = "http://hq.sinajs.cn/list"
 
 // GetData return real time stock data info
@@ -10,5 +14,7 @@ func GetData(sotck string) Data {
 
 // TushareToSina convert Stock name from tushare format to Sina format
 func TushareToSina(stock string) string {
+	t := strings.Split(stock, ".")
+	stock = strings.ToLower(t[1]) + t[0]
 	return stock
 }
