368b558d5b396ec4cdeb263acc9744e01c1bf920
rencc <renccn@gmail.com>
2019-10-10 17:27:59 +0800

    complete GetData func

--- src/sina/utils.go
+++ src/sina/utils.go
@@ -1,15 +1,35 @@
 package sina
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"strings"
 )
 
 const sinaURLPrefix = "http://hq.sinajs.cn/list"
 
 // GetData return real time stock data info
-func GetData(sotck string) Data {
-	var data Data
-	return data
+func GetData(stock string) Data {
+	stock = TushareToSina(stock)
+	URL := sinaURLPrefix + stock
+	resp, err := http.Get(URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(resp)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	t := strings.TrimSuffix(string(body), ";")
+	d := strings.Split(t, "\"")
+	var data *Data
+	err = json.Unmarshal([]byte(d[1]), &data)
+	return *data
 }
 
 // TushareToSina convert Stock name from tushare format to Sina format
