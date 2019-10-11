15d210f212ea1fa64b9d6954432bf30bc486037f
charlesren <renccn@gmail.com>
2019-10-11 21:27:38 +0800

    save resp to struct

--- src/sina/utils.go
+++ src/sina/utils.go
@@ -1,11 +1,11 @@
 package sina
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
@@ -15,21 +15,53 @@ const sinaURLPrefix = "http://hq.sinajs.cn/list="
 func GetData(stock string) Data {
 	stock = TushareToSina(stock)
 	URL := sinaURLPrefix + stock
+	fmt.Println(URL)
 	resp, err := http.Get(URL)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(resp)
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-	t := strings.TrimSuffix(string(body), ";")
-	d := strings.Split(t, "\"")
-	var data *Data
-	err = json.Unmarshal([]byte(d[1]), &data)
-	return *data
+	t := strings.Split(string(body), "\"")
+	d := strings.Split(t[1], ",")
+	fmt.Println("Return data is :", d)
+	var data Data
+	data.Name = d[0]
+	data.Open, _ = strconv.ParseFloat(d[1], 64)
+	data.Close, _ = strconv.ParseFloat(d[2], 64)
+	data.Now, _ = strconv.ParseFloat(d[3], 64)
+	data.High, _ = strconv.ParseFloat(d[4], 64)
+	data.Low, _ = strconv.ParseFloat(d[5], 64)
+	data.Buy, _ = strconv.ParseFloat(d[6], 64)
+	data.Sell, _ = strconv.ParseFloat(d[7], 64)
+	data.Turnover, _ = strconv.Atoi(d[8])
+	data.Volume, _ = strconv.ParseFloat(d[9], 64)
+	data.Bid1Volume, _ = strconv.Atoi(d[10])
+	data.Bid1, _ = strconv.ParseFloat(d[11], 64)
+	data.Bid2Volume, _ = strconv.Atoi(d[12])
+	data.Bid2, _ = strconv.ParseFloat(d[13], 64)
+	data.Bid3Volume, _ = strconv.Atoi(d[14])
+	data.Bid3, _ = strconv.ParseFloat(d[15], 64)
+	data.Bid4Volume, _ = strconv.Atoi(d[16])
+	data.Bid4, _ = strconv.ParseFloat(d[17], 64)
+	data.Bid5Volume, _ = strconv.Atoi(d[18])
+	data.Bid5, _ = strconv.ParseFloat(d[19], 64)
+	data.Ask1Volume, _ = strconv.Atoi(d[20])
+	data.Ask1, _ = strconv.ParseFloat(d[21], 64)
+	data.Ask2Volume, _ = strconv.Atoi(d[22])
+	data.Ask2, _ = strconv.ParseFloat(d[23], 64)
+	data.Ask3Volume, _ = strconv.Atoi(d[24])
+	data.Ask3, _ = strconv.ParseFloat(d[25], 64)
+	data.Ask4Volume, _ = strconv.Atoi(d[26])
+	data.Ask4, _ = strconv.ParseFloat(d[27], 64)
+	data.Ask5Volume, _ = strconv.Atoi(d[28])
+	data.Ask5, _ = strconv.ParseFloat(d[29], 64)
+	data.Date = d[30]
+	data.Time = d[31]
+	return data
 }
 
 // TushareToSina convert Stock name from tushare format to Sina format
