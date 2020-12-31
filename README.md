fb4bc2937bd3a17e4cc98a0ba850d9e6a5b8d3b7
root <root@redhat82.cs.mgmt>
2020-12-31 16:25:51 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+#### quant-tushare
+
+quant-tushare is used to save tushare data into database .
--- main.go
+++ main.go
@@ -7,17 +7,24 @@ import (
 )
 
 func main() {
+	//init database
 	db := config.GetDB()
 	defer db.Close()
+	//init tushare account
 	api := tushare.New(tushare.TushareConfig.Token)
+	//define api params
 	params := make(tushare.Params)
+	//define api response  field
 	var fields tushare.Fields
 	fields = tushare.APIFullFields["stock_basic"]
+	//request date from tushare
 	resp, err := api.GetTushareData("stock_basic", params, fields)
 	if err != nil {
 		log.Fatal(err)
 	}
+	//print response fields
 	resp.TushareModelFields()
+	//request data and save into db
 	tushare.UpdateTradeCal(db, api)
 	tushare.UpdateStockBasic(db, api)
 	tushare.UpdateDaily(db, api)
